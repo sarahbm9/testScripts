4f09382441c84167ae234d6182093535e15f78a7
cpoythress <pcarter7@vt.edu>
2020-12-01 15:24:34 -0500

    first working draft of nav script

--- nav.py
+++ nav.py
@@ -1,22 +1,56 @@
 import paho.mqtt.client as mqtt
 import math
 import time
+import json
+
+
+#I'm just storing the current state in nav.curState as a raw int to keep things simple
+
 class Navigation:
     def __init__(self):
-        self.currentPos = (0,0)
-        self.currentMove = (3,0)
-        self.goalPos = (10,30)
-        self.goalPosPolar = (math.tan(self.goalPos(1)/self.goalPos(0)), math.sqrt(self.goalPos(0)**2 + self.goaPos(1)**2)
-        
-    def decideMovement(self, distance, angle):
-        #THIS WILL DECIDE NEXT MOVE need to update currentPos, and curentMove
-        maxD = max(distance)
-        maxIndex = distance.index(maxD)
+        self.curState = 0   #states: SEARCH_1 = 0, SEARCH_2 = 1, MOVE = 2
+        self.prevState = 0
+        self.roverReady = 1 #tracks whether or not the rover is ready to received a new nav instruction
+        self.curAngle = 0   #current angle that will be sent to the rover in the next message
+        self.prevAngle = 0  #previous angle that was received from movement.py
+        self.curDist = 0    #current distance that will be sent to the rover in the next message
+        self.prevDist = 0   #previous distance that was received from movement.py
 
+    def no_goal_found(self, input_vals):         #state transitions for when no goal has been found
+        self.goalFound = 0
+        self.roverReady = 0
+        if self.curState == 0:                 #if curstate is SEARCH_1
+            if self.prevState == 1:            #if rover just turned back because goal still not found
+                self.curAngle = self.prevAngle
+                self.curDist = self.prevDist
+                self.prevState = self.curState
+                self.curState = 2
+            else:
+                self.prevAngle = input_vals["angle"]  #store suggested angle and distance for potential use later
+                self.curAngle = 180
+                self.prevDist = input_vals["distance"]
+                self.curDist = 0
+                self.prevState = self.curState
+                self.curState = 1               #next state will be SEARCH_2
 
+        elif self.curState == 1:  #WHAT BEHAVIOR DO WE WANT IN THIS CASE?
+            self.prevState = self.curState
+            self.curState = 0
+            self.curAngle = -180
+            self.curDist = 0
 
-    
+        elif self.curState == 2:                  #if curstate is MOVE
+            self.curState = 0                     #next state will be SEARCH_1
 
+    def goal_found(self, input_vals):            #There should only be one state transition when the goal is found
+        self.goalFound = 1
+        self.roverReady = 0
+        self.prevAngle = self.curAngle
+        self.curAngle = input_vals["angle"]
+        self.prevDist = self.curDist
+        self.curDist = input_vals["distance"]
+        self.prevState = self.curState
+        self.curState = 2
 
 class MQTThandler:
     def __init__(self):
@@ -26,10 +60,12 @@ class MQTThandler:
         self.client.username_pw_set("dhnngvfj", "zhM1Ds0tjbnC")
         self.client.connect("m16.cloudmqtt.com", port=18367)
         self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
+        #self.client.on_message = self.on_message
+        self.client.message_callback_add("cc32xx/mapOut", self.on_map_message)
+        self.client.message_callback_add("cc32xx/RoverReady", self.on_rover_message)
 
         self.client.loop_start()
-        self.client.subscribe("cc32xx/sMsg")
+        self.client.subscribe("cc32xx/#")
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
@@ -38,16 +74,27 @@ class MQTThandler:
         else:
             print("connection failed")
 
-    def on_message(self,client, userdata, message): 
-        print("message recieved: ")
-        angle_val = json.loads(message.payload.decode('utf-8'))
-        #print(angle_val)
-        angle_hold = [0.0,np.pi/6,np.pi/3,np.pi/2,2*np.pi/3,5*np.pi/6,np.pi]
-        distance_hold = [angle_val["d0"], angle_val["d30"], angle_val["d60"], angle_val["d90"], angle_val["d120"], angle_val["d150"], angle_val["d180"]]
-        isGoal_hold = [angle_val["isG0"], angle_val["isG30"], angle_val["isG60"], angle_val["isG90"], angle_val["isG120"], angle_val["isG150"], angle_val["isG180"]]
-        angle_hold = [angle_val["ang0"]*np.pi/180, angle_val["ang30"]*np.pi/180, angle_val["ang60"]*np.pi/180, angle_val["ang90"]*np.pi/180, angle_val["ang120"]*np.pi/180, angle_val["ang150"]*np.pi/180, angle_val["ang1800"]*np.pi/180 ]
-        self.nav.decideMovement(distance_hold, angle_hold)
-        self.client.publish("cc32xx/navigation", '{"distance": '+str(self.nav.curentMove(0))', "angle":'+str(self.nav.curentMove(1))+'}')
+    def on_map_message(self,client, userdata, message):
+        print("map message recieved...")
+        input_vals = json.loads(message.payload.decode('utf-8'))
+        #print(input_vals)
+        if self.nav.roverReady == 1:
+            if input_vals["state"] == "no goal found":
+                self.nav.no_goal_found(input_vals)
+            else:
+                self.nav.goal_found(input_vals)
+            self.client.publish("cc32xx/navigation", '{"distance": '+str(self.nav.curDist)+', "angle" :'+str(self.nav.curAngle)+'}')
+            self.client.publish("TR/navScript", '{"curState": '+str(self.nav.curState)+', "prevState": '+str(self.nav.prevState)+', "RoverReady": '+str(self.nav.roverReady)+', "curAngle": '+str(self.nav.curAngle)+', "prevAngle": '+str(self.nav.prevAngle)+', "curDist": '+str(self.nav.curDist)+', "prevDist": '+str(self.nav.prevDist)+'}')
+
+    def on_rover_message(self, client, userdata, message):
+        print("rover ready message received...")
+        self.nav.roverReady = 1
+        if self.nav.curState == 2:
+            self.nav.prevState = self.nav.curState
+            self.nav.curState = 0
+        self.client.publish("TR/navScript", '{"curState": '+str(self.nav.curState)+', "prevState": '+str(self.nav.prevState)+', "RoverReady": '+str(self.nav.roverReady)+', "curAngle": '+str(self.nav.curAngle)+', "prevAngle": '+str(self.nav.prevAngle)+', "curDist": '+str(self.nav.curDist)+', "prevDist": '+str(self.nav.prevDist)+'}')
 
 if __name__ == "__main__":
     mt = MQTThandler()
+    while 1:
+        time.sleep(0.1)
